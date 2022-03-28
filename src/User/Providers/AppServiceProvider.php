d25b090bb2a2758650a0306aabf9b738225ae07f
Vitalik <petergerinv@gmail.com>
2022-03-28 13:26:10 +0300

    feat: Configure factory paths.

--- src/User/Providers/AppServiceProvider.php
+++ src/User/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Freelance\User\Providers;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,18 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->register(EventServiceProvider::class);
         $this->app->register(RouteServiceProvider::class);
+
+        Factory::guessFactoryNamesUsing(function (string $modelName) {
+            return explode('\Provider', __NAMESPACE__)[0] .
+                   '\Infrastructure\Database\Factories\\' .
+                   (class_basename($modelName)) .
+                   'Factory';
+        });
+
+        Factory::guessModelNamesUsing(function (object $factoryClass) {
+            return explode('\Provider', __NAMESPACE__)[0] .
+                   '\Domain\Models\\' .
+                   explode('Factory', (class_basename($factoryClass)))[0];
+        });
     }
 }
