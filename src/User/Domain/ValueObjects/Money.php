64b2b0bfa349c61704ebb87bbdb7fd45cd55b9d8
Vitalik <petergerinv@gmail.com>
2022-04-04 16:49:01 +0300

    feat: Add Money ValueObject.

---
+++ src/User/Domain/ValueObjects/Money.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Freelance\User\Domain\ValueObjects;
+
+use InvalidArgumentException;
+
+final class Money
+{
+    private function __construct(private int $amount)
+    {
+        if ($amount < 0) {
+            throw new InvalidArgumentException(
+                'Amount ' . $amount . ' is negative number.'
+            );
+        }
+    }
+
+    public static function create(int $amount)
+    {
+        return new self($amount);
+    }
+
+    public function value(): string
+    {
+        return $this->amount;
+    }
+
+    public function equals(Money $other): bool
+    {
+        return $this->amount === $other->amount;
+    }
+}
