24c1affefdd8c2a6fcb13aee9e4109e3fb2e4f4a
Vitalik <petergerinv@gmail.com>
2022-04-11 14:05:02 +0300

    feat: Pest setup parallel and fix configs.

--- src/User/Domain/Actions/Fortify/ResetUserPassword.php
+++ src/User/Domain/Actions/Fortify/ResetUserPassword.php
@@ -5,26 +5,25 @@ namespace Freelance\User\Domain\Actions\Fortify;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\ResetsUserPasswords;
+use Laravel\Fortify\Rules\Password;
 
 class ResetUserPassword implements ResetsUserPasswords
 {
-    use PasswordValidationRules;
-
     /**
      * Validate and reset the user's forgotten password.
      *
-     * @param  mixed  $user
-     * @param  array  $input
+     * @param mixed $user
+     * @param array $input
      * @return void
      */
     public function reset($user, array $input)
     {
         Validator::make($input, [
-            'password' => $this->passwordRules(),
+            'password' => ['required', 'string', new Password, 'confirmed'],
         ])->validate();
 
         $user->forceFill([
-            'password' => Hash::make($input['password']),
-        ])->save();
+                             'password' => Hash::make($input['password']),
+                         ])->save();
     }
 }
